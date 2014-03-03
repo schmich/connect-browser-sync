35a98794ea0c4b0dd70f22e5d7b4e965ed43a785
Chris Schmich <schmch@gmail.com>
2014-03-02 20:29:57 -0700

    Making code more terse, clarifying some names.

--- index.js
+++ index.js
@@ -3,23 +3,19 @@
 
 var injector = require('connect-injector');
 
-function injectBrowserSync(browserSyncServer) {
+module.exports = function injectBrowserSync(browserSyncServer) {
   var snippet = '';
 
   browserSyncServer.on('init', function(api) {
     snippet = api.snippet;
   });
 
-  var inject = injector(function(req, res) {
-    var header = res.getHeader('content-type');
-    return header && (header.toLowerCase().indexOf('text/html') >= 0);
-  }, function(callback, data, req, res) {
+  return injector(function(req, res) {
+    var contentType = res.getHeader('content-type');
+    return contentType && (contentType.toLowerCase().indexOf('text/html') >= 0);
+  }, function(callback, content, req, res) {
     var lastBody = /<\s*\/\s*body\s*>(?!(.|\n)*<\s*\/\s*body\s*>)/gi;
-    var injected = data.toString().replace(lastBody, snippet + '</body>');
+    var injected = content.toString().replace(lastBody, snippet + '</body>');
     callback(null, injected);
   });
-
-  return inject;
-}
-
-module.exports = injectBrowserSync;
+};
