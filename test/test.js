6af1ddb0a97f9f0be52dc0ff74a7352eb0a94eed
Chris Schmich <schmch@gmail.com>
2017-02-01 01:20:24 -0600

    Add test for <head> injection.

--- test/test.js
+++ test/test.js
@@ -9,29 +9,56 @@ var request = require('request');
 
 var content = '7d0b94d0-c6ec-11e4-8830-0800200c9a66';
 
-var bs = browserSync({ logSnippet: false, host: '127.0.0.1' });
-var app = connect().use(connectBrowserSync(bs)).use(function(req, res) {
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body>' + content + '</body></html>');
-});
+function createApp(options = {}) {
+  var bs = browserSync.create().init({ logSnippet: false, host: '127.0.0.1' });
+  return connect().use(connectBrowserSync(bs, options)).use(function (req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><head><title>Test</title></head><body>' + content + '</body></html>');
+  });
+}
 
-describe('connect-browser-sync', function() {
-  it('serves a page with injected tags', function(done) {
+describe('connect-browser-sync', function () {
+  it('serves a page with tags injected into <body>', function (done) {
     this.timeout(30 * 1000);
 
+    var app = createApp();
     var port = 9001;
-    var server = http.createServer(app).listen(port, function() {
-      request('http://127.0.0.1:' + port, function(err, response, body) {
+    var server = http.createServer(app).listen(port, function () {
+      request('http://127.0.0.1:' + port, function (err, response, payload) {
         if (err) {
           assert.fail('Error in request.');
         }
 
-        if (body.indexOf(content) < 0) {
+        if (payload.indexOf(content) < 0) {
           assert.fail('"' + content + '" not found in response.');
         }
 
-        if (!/<script.*>/i.test(body)) {
-          assert.fail('<script> tag not found in response.');
+        if (!/<body>.*?<script[^]*?>[^]*?<\/body>/i.test(payload)) {
+          assert.fail('<script> tag not found in <body>.');
+        }
+
+        done();
+      });
+    });
+  });
+
+  it('serves a page with tags injected into <head>', function (done) {
+    this.timeout(30 * 1000);
+
+    var app = createApp({ injectHead: true });
+    var port = 9002;
+    var server = http.createServer(app).listen(port, function () {
+      request('http://127.0.0.1:' + port, function (err, response, payload) {
+        if (err) {
+          assert.fail('Error in request.');
+        }
+
+        if (payload.indexOf(content) < 0) {
+          assert.fail('"' + content + '" not found in response.');
+        }
+
+        if (!/<head>.*?<script[^]*?>[^]*?<\/head>/i.test(payload)) {
+          assert.fail('<script> tag not found in <head>.');
         }
 
         done();
